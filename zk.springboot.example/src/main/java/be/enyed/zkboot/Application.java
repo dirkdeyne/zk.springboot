30008bf5a9b8c2eca639402e03da1d481dd61bb5
Dirk Deyne <dirkdeyne@gmail.com>
2017-12-01 22:56:15 +0100

    example using @EnableZk

---
+++ zk.springboot.example/src/main/java/be/enyed/zkboot/Application.java
@@ -0,0 +1,13 @@
+package be.enyed.zkboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableZk
+public class Application {
+
+  public static void main(String[] args) {
+	SpringApplication.run(Application.class, args);
+  }
+}
